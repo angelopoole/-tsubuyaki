b13b799774f8e07e72c52a5676b9a84ef262b294
angelopoole <angelo.poole@outlook.com>
2021-04-01 23:02:43 -0400

    var add

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
 import { getUsers, getUserById } from '../controllers/userController.js';
-
+// :id = var
 router.route('/').get(getUsers);
 
 router.route('/:id').get(getUserById);
